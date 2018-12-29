9216c5c55e55e7c06a35c63fc928e18f138350be
unknown <nbatre1@yahoo.com>
2018-12-29 07:02:19 -0600

    this is the final update for the project

--- js/app.js
+++ js/app.js
@@ -155,7 +155,16 @@ class UI {
    }
 // delete expense
    deleteExpense(element){
-
+     let id =parseInt(element.dataset.id);
+     let parent =element.parentElement.parenetElement.parentElement;
+     // removes from the Dom
+     this.expenseList.removeChild(parent);
+    // removes from the list
+    let tempList = this.itemList.filter(function(item){
+       return item.id !== id;
+    });
+    this.itemList = tempList;
+    this.showBalance();
    }
 }
 
